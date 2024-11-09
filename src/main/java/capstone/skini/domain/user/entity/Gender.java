836ea8ed9fb6c44d9055ada5297ce8c3df743073
aspyo <sungpyo8334@naver.com>
2024-11-10 00:12:04 +0900

    [FEAT] : User Entity 생성
    1. 유저 엔티티 생성
    2. BaseEntity 생성
    3. Enum 타입 Gender, LoginType 생성

---
+++ src/main/java/capstone/skini/domain/user/entity/Gender.java
@@ -0,0 +1,5 @@
+package capstone.skini.domain.user.entity;
+
+public enum Gender {
+    MALE, FEMALE, OTHER
+}
---
+++ src/main/java/capstone/skini/domain/user/entity/LoginType.java
@@ -0,0 +1,5 @@
+package capstone.skini.domain.user.entity;
+
+public enum LoginType {
+    OUR, SOCIAL
+}
