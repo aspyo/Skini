fd5333fb0d8f4226262d97edba3bcad13413099a
aspyo <sungpyo8334@naver.com>
2024-11-10 00:40:29 +0900

    [FEAT] : 스프링 시큐리티 FilterChain 생성
    1. Spring Security FilterChain 설정 객체 생성

---
+++ src/main/java/capstone/skini/security/config/SecurityConfig.java
@@ -0,0 +1,78 @@
+package capstone.skini.security.config;
+
+import capstone.skini.domain.refresh.repository.RefreshTokenRepository;
+import capstone.skini.security.entrypoint.CustomAuthenticationEntryPoint;
+import capstone.skini.security.filter.JWTFilter;
+import capstone.skini.security.filter.LoginFilter;
+import capstone.skini.security.handler.CustomOAuth2SuccessHandler;
+import capstone.skini.security.jwt.JWTUtil;
+import capstone.skini.security.service.CustomOAuth2UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final JWTUtil jwtUtil;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final CustomAuthenticationEntryPoint authenticationEntryPoint;
+
+    // OAuth2 컴포넌트
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final CustomOAuth2SuccessHandler customOAuth2SuccessHandler;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .formLogin(form -> form.disable())
+                .httpBasic(basic -> basic.disable())
+                .logout(logout -> logout.disable())
+                .csrf(csrf -> csrf.disable());
+
+        http
+                .oauth2Login(oauth -> oauth
+                        .userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig
+                                .userService(customOAuth2UserService))
+                        .successHandler(customOAuth2SuccessHandler))
+                .exceptionHandling(exception -> exception.authenticationEntryPoint(authenticationEntryPoint));
+
+
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/login", "/api/join", "/api/reissue").permitAll()
+                        .anyRequest().authenticated());
+
+        http
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http
+                .addFilterBefore(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil, refreshTokenRepository),
+                        UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new JWTFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+}
