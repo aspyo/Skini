721d1d89a050c56489d5febbf2ea52b035be58da
aspyo <sungpyo8334@naver.com>
2024-11-10 00:39:38 +0900

    [FEAT] : 로그인 성공/실패 핸들러 구현
    1. 로그인 성공/실패시 호출되는 Success/Failure 핸들러 클래스 생성
    
    2. 로그인 권한이 필요한 api에 접근할때 로그인이 안돼있을경우 처리하는 EntryPoint 구현

---
+++ src/main/java/capstone/skini/security/handler/CustomOAuth2FailureHandler.java
@@ -0,0 +1,19 @@
+package capstone.skini.security.handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CustomOAuth2FailureHandler extends SimpleUrlAuthenticationFailureHandler {
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, exception.getMessage());
+        System.out.println("OAuth2 로그인에 실패하였습니다.");
+    }
+}
