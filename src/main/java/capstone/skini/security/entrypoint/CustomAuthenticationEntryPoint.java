27141e4c55d75e567563b95d258aa8eb63d6820b
aspyo <sungpyo8334@naver.com>
2024-11-12 04:13:34 +0900

    [FIX] : 컨트롤러 에러 수정
    1. 시큐리티 필터를 통과 후 컨트롤러까지 갔을때 에러가 발생할 경우, 스프링은 자동으로 /error 로 요청을 함. 이때 시큐리티 필터에 /error 를 permitAll 해줘야 제대로 오류처리가 진행

--- src/main/java/capstone/skini/security/entrypoint/CustomAuthenticationEntryPoint.java
+++ src/main/java/capstone/skini/security/entrypoint/CustomAuthenticationEntryPoint.java
@@ -13,16 +13,13 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
-@Component
-@RequiredArgsConstructor
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
-
-    private final ObjectMapper objectMapper;
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setStatus(HttpStatus.UNAUTHORIZED.value());
-        response.getWriter().write("no login");
+        response.getWriter().write("no login!!!");
     }
 }
