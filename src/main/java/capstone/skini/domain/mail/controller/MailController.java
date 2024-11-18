d948055051429ad834a80f8bd8426078f50b18bc
aspyo <sungpyo8334@naver.com>
2024-11-18 16:23:10 +0900

    [FEAT] : 이메일 인증 일부 구현
    1. 이메일 인증 코드 발급 후 이메일로 전송되는 것까지 구현 및 테스트 성공

---
+++ src/main/java/capstone/skini/domain/mail/controller/MailController.java
@@ -0,0 +1,16 @@
+package capstone.skini.domain.mail.controller;
+
+import capstone.skini.domain.mail.service.MailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class MailController {
+
+    private final MailService mailService;
+
+}
